368de632948a803a84be3f10d1d481c8c9abe1d4
Rachel Wellington <rwelling@fredhutch.org>
2024-09-12 15:33:39 -0700

    updated iPS_10X_preprocessing.R

--- figure-02_supp-figure-02_iPS-10X/iPS_10X_preprocessing.R
+++ figure-02_supp-figure-02_iPS-10X/iPS_10X_preprocessing.R
@@ -8,8 +8,13 @@ library(Matrix)
 
 #Note that there are areas in this code that contain underlying variability
 #that may lead to slight variations in the distribution of cells in UMAP space
+mat_path <- "~/cellranger/iPS_10X/matrix.mtx.gz"
+feature_anno_path <- "~/cellranger/iPS_10X/features.tsv.gz"
+cell_anno_path <- "~/cellranger/iPS_10X/barcodes.tsv.gz"
 
-cds <- load_cellranger_data("~\cellranger\iPS_10X")
+cds <- load_mm_data(mat_path = mat_path, 
+                    feature_anno_path = feature_anno_path, 
+                    cell_anno_path = cell_anno_path)
 
 #Add a column to the metadata containing the number of UMIs for each cell
 colData(cds)$n.umi <- Matrix::colSums(counts(cds))
@@ -32,4 +37,6 @@ cds <- reduce_dimension(cds)
 cds_clust <- cluster_cells(cds, resolution = 3.5e-3)
 
 #Look at cells in UMAP
+#Note that there is a random element in one of the monocle3 functions that may
+#lead to some differences
 plot_cells(cds_clust)
